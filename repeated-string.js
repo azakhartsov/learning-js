88acb6a8495ff9f0a8be4cf9896fce402b887742
Andrew <nikamoda@gmail.com>
2021-05-05 22:20:03 +1000

    partly working

--- repeated-string.js
+++ repeated-string.js
@@ -1,14 +1,13 @@
-let s = "a";
-let n = 1000000000000;
+let s = "abcac";
+let n = 10;
 let sum = 0n;
 
 function repeatedString(s, n) {
    let i = 0;
-   s = s.repeat(n);
-   console.log(s);
+   s = s.repeat(n); //тупо клонирую заданную строку n раз
    do {
-      sum = s.slice(0,n).split('a').length - 1;
-      console.log(`Sliced arr ${sum}`);
+      sum = s.slice(0,n).split('a').length - 1; // отрезаю n символов из получившейся строки и считаю количество А
+      console.log(sum);
    }
    while (i = 0, i < n, i++);
 }
